1b57ece2f4c35327cebae4ab8bc2b88fae415fb0
Muzafar Ali <muzafar.ali.chandio@gmail.com>
2025-07-29 02:41:25 +0500

    changed connectdb fun code

--- src/utils/connectDatabase.ts
+++ src/utils/connectDatabase.ts
@@ -1,14 +1,30 @@
-import { database } from "../config/dataBase";
+// import { database } from "../config/dataBase";
 
-const connectDatabase = async () => {
+// const connectDatabase = async () => {
+//   try {
+//     await database.connect();
+//     return true;
+
+//   } catch (error) {
+//     console.error('Failed to connect to database:', error);
+//     return false;
+//   }
+// }
+
+// export default connectDatabase;
+
+import mongoose from "mongoose"
+import config from "../config/config.js";
+
+export const connectDB = async () => {
   try {
-    await database.connect();
-    return true;
-
+    const { connection } = await mongoose.connect(config.mongoURI as string);
+    console.log(`MongoDB connected with ${connection.host}`);
+    
+    return connection.host;
+    
   } catch (error) {
-    console.error('Failed to connect to database:', error);
-    return false;
+    console.log('DB connection :',error);
+    process.exit(1);
   }
 }
-
-export default connectDatabase;
