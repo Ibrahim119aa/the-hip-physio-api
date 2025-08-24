3fc48c5af9edcb02f53861b1a4511e1a65fcb155
Muzafar-Ali <84478313+Muzafar-Ali@users.noreply.github.com>
2025-08-24 17:04:47 +0500

    feat: added auto jobs, notification controllers

--- src/routes/notification.routes.ts
+++ src/routes/notification.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import NotificationModel from "../models/notifications.model";
+import { isAdminAuthenticated } from "../middlewares/isAuthenticated.middleware";
+import { cancelNotificationHandler, createAndSheduleNotificationHandler, getNotificationListHandler } from "../controllers/notification.controllers";
 
 const router = Router();
 
-router.route("/").get(async (req, res) => {
-  await NotificationModel.create();
 
-  res.send("Hello World!");
-});
+router.route("/")
+  .post(isAdminAuthenticated, createAndSheduleNotificationHandler)
+  .get(isAdminAuthenticated, getNotificationListHandler);
+
+router.route('/:notificationId/cancel').post(isAdminAuthenticated, cancelNotificationHandler)
 
 export default router;
